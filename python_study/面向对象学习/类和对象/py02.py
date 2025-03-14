14b92c3d768f6484a5033dc82114b942c573e947
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-14 20:40:50 +0800

    类和对象

---
+++ python_study/面向对象学习/类和对象/py02.py
@@ -0,0 +1,22 @@
+# 类和对象
+# 基于类创建对象：对象名 = 类名称()
+# 类相当于程序内的'设计图纸'，需要基于图纸生产实体(对象)，才能正常工作：面向对象编程
+# 设计一个闹钟类
+# class Clock:
+#     id = None
+#     price = None
+#
+#     def ring(self):
+#         import winsound
+#         winsound.Beep(2000, 3000)
+# # 构建两个闹钟
+# clock1 = Clock()
+# clock1.id = "001"
+# clock1.price = 19.9
+# print(f"闹钟1的id是{clock1.id},价格是{clock1.price}")
+# clock1.ring()
+# clock2 = Clock()
+# clock2.id = "002"
+# clock2.price = 29.9
+# print(f"闹钟2的id是{clock2.id},价格是{clock2.price}")
+# clock2.ring()
