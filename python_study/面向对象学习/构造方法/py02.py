5b7cde4df75d142a49dd5c290b1dad8545da848b
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-14 21:24:58 +0800

    构造方法

---
+++ python_study/面向对象学习/构造方法/py02.py
@@ -0,0 +1,16 @@
+# Python类可以使用：__init__()方法，称之为构造方法
+# 可以实现：
+# 在创建类对象(构造类)的时候，会自动执行
+# 在创建类对象(构造类)的时候，将参数自动传递给_init_方法使用
+# class Student:
+#     # 可以省略
+#     # name = None
+#     # age = None
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print("Student类创建了一个对象")
+#
+# stu = Student("奶龙", 18)
+# print(stu.name, stu.age)
