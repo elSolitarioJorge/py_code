1e34aa1ab6ec5bc6ce5a6e93d633f6cf80230180
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-15 11:48:13 +0800

    补充

--- python_study/面向对象学习/封装/py02.py
+++ python_study/面向对象学习/封装/py02.py
@@ -30,3 +30,23 @@
 # # print(phone.__current_voltage) # 报错，无法访问
 # # phone.__keep_single_core()     # 报错，无法访问
 # phone.call_by_5g()
+
+# 私有成员的意义：
+# 在类中提供仅供内部使用的属性和方法、而不对外开放（类对象无法使用）
+
+# 设计一个类，用来描述手机
+# class Phone:
+#     # 提供私有成员变量：__is_5g_enable
+#     __is_5g_enable = False
+#
+#     # 提供私有成员方法：__check_5g()
+#     def __check_5g(self):
+#         if self.__is_5g_enable:
+#             print("5g开启")
+#         else:
+#             print("5g关闭，使用4g网络")
+#     def call_by_5g(self):
+#         self.__check_5g()
+#         print("正在通话中")
+# phone = Phone()
+# phone.call_by_5g()
